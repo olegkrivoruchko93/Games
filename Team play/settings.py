d048e31640dbd1ce9add7faf9a85acea9ba4d3cf
olegkrivoruchko93 <90267901+olegkrivoruchko93@users.noreply.github.com>
2023-04-02 12:30:18 +0300

    update
    update

--- Team play/settings.py
+++ Team play/settings.py
@@ -1,8 +1,9 @@
-FPS = 60
+FPS = 70
 PLAYER_VEL = 5
 PLAYER_MAX_HEALT = 5
 CURRENT_LEVEL = 0
 LEVELS = 15
 WIDTH, HEIGHT = 1000, 800 # screan width and height
+SCROLL_AREA_DIDTH, SCROLL_AREA_HEIGHT = 300, 300
 
-MainCharacters = ["MaskDude", "NinjaFrog", "PinkMan", "VirtualGuy"]
+MainCharacters = ("MaskDude", "NinjaFrog", "PinkMan", "VirtualGuy")
